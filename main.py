ba545f8c2f2c5b1b32658cb9721533f842c45a72
misterabs26 <mister.abs26@gmail.com>
2025-03-06 18:39:11 +0800

    Added an upload feature

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import streamlit as st
 from PIL import Image
 
+st.subheader("Colored image to Grayscale Converter")
+
+upload_image = st.file_uploader("Upload Image")
+
 with st.expander("Start Camera"):
     camera_img = st.camera_input("Camera")
 
-if camera_img:
+
+def grayscale_img(photo):
     # Create a pillow image instance
-    img = Image.open(camera_img)
+    img = Image.open(photo)
 
     gray_img = img.convert("L")
     st.image(gray_img)
+
+if camera_img:
+    grayscale_img(camera_img)
+
+if upload_image:
+    grayscale_img(upload_image)
+
+
+
+
+# When the user uploads an image
